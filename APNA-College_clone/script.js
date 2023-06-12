2e1d5e1f03ee71a1370ee5b9186102911d7ea3ca
RAHUL <dasr16583@gmail.com>
2023-06-12 19:25:21 +0530

    Add files via upload

---
+++ APNA-College_clone/script.js
@@ -0,0 +1,40 @@
+// prevent the default  scroll
+// document.addEventListener("scroll", function (event) {
+//   event.preventDefault();
+// });
+
+// for animate text
+var typed = new Typed("#auto-type", {
+  strings: ["Javascript ?", "Web Development ?", "Mern Stack ?"],
+  typeSpeed: 120,
+  backSpeed: 120,
+  loop: true,
+});
+
+// animate number
+const counterNum = document.querySelectorAll(".counter-numbers");
+
+const speed = 200;
+
+counterNum.forEach((curElem) => {
+  const updateNumber = () => {
+    const targetNumber = parseInt(curElem.dataset.number);
+    const initialNum = parseInt(curElem.innerText);
+
+    const incrementNumber = Math.trunc(targetNumber / speed);
+    // console.log(incrementNumber);
+    if (initialNum < targetNumber) {
+      curElem.innerText = `${initialNum + incrementNumber} +`;
+      setTimeout(updateNumber, 12);
+    }
+  };
+
+  updateNumber();
+});
+
+// for slider
+// const slides = document.querySelectorAll(".animate-first");
+// console.log(slides);
+// slides.forEach((animateFirst, index) => {
+//   animateFirst.computedStyleMap.left = `${index * 100}`;
+// });
